96447f9e17373cbe7aeb327f6654e55aa25b44cb
Chris-Merriman-dev <chris.merriman78@gmail.com>
2026-03-30 17:55:16 -0400

    found issue i think?

--- ocp/test_selenium.py
+++ ocp/test_selenium.py
@@ -16,15 +16,17 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.options import Options
+
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+#from selenium.webdriver.firefox.service import Service
+#from webdriver_manager.firefox import GeckoDriverManager
 
 from typing import Any
 import time
